9976b15b875783cf1231071697f05285529185fa
Usman Ali <uat@journeyxp.com>
2018-07-11 15:58:58 +0500

    first commit

---
+++ src/main/java/com/test/project/model/PackageInfo.java
@@ -0,0 +1,112 @@
+package com.test.project.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table ( name="package_info" )
+public class PackageInfo implements Serializable{
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private double price;
+	
+	@Column
+	private boolean isActive;
+	
+	@Column
+	private String description;
+	
+	@Column
+	private int duration;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column (name ="created_at")
+	private Date createdAt;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column (name ="updated_at")
+	private Date updatedAt;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/test/project/repository/PackageInfoRepository.java
@@ -0,0 +1,16 @@
+package com.test.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.test.project.model.PackageInfo;
+
+/**
+ * 
+ * @author usman
+ *
+ */
+@Repository
+public interface PackageInfoRepository extends JpaRepository<PackageInfo, Integer>{
+
+}
